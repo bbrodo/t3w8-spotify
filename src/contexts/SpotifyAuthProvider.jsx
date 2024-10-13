6333be370a791632be526acdc985273f294ce8e0
bbrodo <15292@coderacademy.edu.au>
2024-10-13 15:26:55 +1100

    feat: completed spotify auth tokens

--- src/contexts/SpotifyAuthProvider.jsx
+++ src/contexts/SpotifyAuthProvider.jsx
@@ -14,7 +14,7 @@ export function useSpotifyAuthContext() {
   return useContext(SpotifyAuthContext);
 }
 
-const clientid = "d39452b7003a4b35b932c634bcc68851";
+const clientId = "d39452b7003a4b35b932c634bcc68851";
 
 export function SpotifyAuthProvider({ children }) {
   // code required for spotify sign in
@@ -32,7 +32,7 @@ export function SpotifyAuthProvider({ children }) {
 
   useEffect(() => {
     async function getAuthData() {
-      const authData = await getAuthTokens(clientid, userAuthCode);
+      const authData = await getAuthTokens(clientId, userAuthCode);
       setUserAuthData(authData);
       window.history.replace(null, "Spotify Statsboards", "/");
     }
@@ -41,11 +41,11 @@ export function SpotifyAuthProvider({ children }) {
     }
   }, [userAuthCode]);
 
-  async function getAuthTokens(clientid, code) {
+  async function getAuthTokens(clientId, code) {
     const verifier = localStorage.getItem("verifier");
 
     const params = new URLSearchParams();
-    params.append("client_id", clientid);
+    params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
     params.append("redirect_uri", "http://localhost:5173/spotifycallback");
@@ -61,8 +61,44 @@ export function SpotifyAuthProvider({ children }) {
     return authTokens;
   }
 
+  async function redirectToAuthCodeFlow() {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:5173/spotifycallback");
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+function generateCodeVerifier(length) {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
   return (
-    <SpotifyAuthContext.Provider value={{ userAuthData }}>
+    <SpotifyAuthContext.Provider value={{ userAuthData, redirectToAuthCodeFlow }}>
       {children}
     </SpotifyAuthContext.Provider>
   );
