21332b203ccc00b61de5cd579930f29ee8589163
bbrodo <15292@coderacademy.edu.au>
2024-10-14 14:29:29 +1100

    make profile data requests

--- src/contexts/SpotifyProfileProvider.jsx
+++ src/contexts/SpotifyProfileProvider.jsx
@@ -1,24 +1,45 @@
-import { createContext, useContext } from "react"
-
+import { createContext, useContext, useEffect, useState } from "react";
+import { useSpotifyAuthContext } from "./SpotifyAuthProvider";
 
 export const defaultProfileData = {
-    userId: "",
-    email: "",
-    uri: "",
-    link: "",
-    profileImage: ""
-}
+  userId: "",
+  email: "",
+  uri: "",
+  link: "",
+  profileImage: "",
+};
 
 export const SpotifyProfileContext = createContext(defaultProfileData);
 
-export function useSpotifyProfileData(){
-    return useContext(SpotifyProfileContext)
+export function useSpotifyProfileData() {
+  return useContext(SpotifyProfileContext);
 }
 
-export function SpotifyProfileProvider({children}){
-    return (
-        <SpotifyProfileContext.Provider>
-            {children}
-        </SpotifyProfileContext.Provider>
-    )
+export function SpotifyProfileProvider({ children }) {
+  let [profileData, setProfileData] = useState(defaultProfileData);
+
+  let { userAuthData } = useSpotifyAuthContext();
+
+  async function fetchProfileData(accessToken) {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return await result.json();
+  }
+
+  useEffect(() => {
+    if (userAuthData && userAuthData.access_token) {
+      fetchProfileData(userAuthData.access_token).then((profileData) => {
+        setProfileData(profileData);
+      });
+    }
+  }, [userAuthData]);
+
+  return (
+    <SpotifyProfileContext.Provider value={profileData}>{children}</SpotifyProfileContext.Provider>
+  );
 }
