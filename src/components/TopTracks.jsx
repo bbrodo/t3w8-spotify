ad419a3fdb39b589b7fb253717976f6c413d28f2
bbrodo <15292@coderacademy.edu.au>
2024-10-14 16:07:54 +1100

    added artist names

--- src/components/TopTracks.jsx
+++ src/components/TopTracks.jsx
@@ -7,14 +7,21 @@ export function TopTracks(){
 
     if (topTracks.items && topTracks.items.length > 0) {
         return(
+            <>
+            <h2>Top Tracks:</h2>
             <div id="topTracksContainer">
                 {topTracks.items.map((track) => {
                     return <div className="trackCard" key={track.id}>
                         <h2>{track.name}</h2>
                         <img src={track.album.images[0].url} alt="" />
+                        <h3>{track.artists.map(artistObj => artistObj.name).join(", " + " ")}</h3>
+                        <button>
+                            <a href={track.external_urls.spotify}>Listen</a>
+                        </button>
                     </div>
                 })}
             </div>
+            </>
         )
     } else {
         return (
